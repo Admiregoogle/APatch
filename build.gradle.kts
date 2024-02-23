cb6e5d1b06bb3064e7921fd7c8e81f61b1c1028f
Admire <admire1971@outlook.com>
2024-02-23 14:40:17 +0800

    back

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,7 +32,7 @@ cmaker {
     }
 }
 
-project.ext.set("kernelPatchVersion", "0.10.0")
+project.ext.set("kernelPatchVersion", "0.9.0")
 
 val androidMinSdkVersion = 26
 val androidTargetSdkVersion = 33
